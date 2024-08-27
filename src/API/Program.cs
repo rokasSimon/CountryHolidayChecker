05b36a37af079d8ec45af1ad4549326683cd2ac5
rokas_simon <rokas.rokanas@gmail.com>
2024-08-27 17:24:26 +0300

    Move swagger from development ENV because Azure requires it

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -20,10 +20,11 @@ builder.Services.AddAPIServices();
 
 var app = builder.Build();
 
+app.UseSwagger();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
